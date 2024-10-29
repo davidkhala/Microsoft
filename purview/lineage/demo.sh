ea378f93d4cb1386c48002fa19fc08d528c41fe8
David Liu <david-khala@hotmail.com>
2024-10-29 22:23:43 +0800

    Update demo.sh

--- purview/lineage/demo.sh
+++ purview/lineage/demo.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
 set -e
 rg=${rg:-"Purview-ADB-Lineage-Solution-Accelerator"} # resource_group
-region=SoutheastAsia                                 # take care of purview
-purviewlocation=$region
+
+purviewlocation=SoutheastAsia
 prefix=""
 
 release=release/2.3
 
+export subscription=$(curl https://raw.githubusercontent.com/davidkhala/azure-utils/refs/heads/main/cli/context.sh | bash -s subscription)
 service_principal=${service_principal:-"Purview-ADB-Lineage-Solution-Accelerator"}
-create-service-principal() {
-    export subscription=$(curl https://raw.githubusercontent.com/davidkhala/azure-utils/refs/heads/main/cli/context.sh | bash -s subscription)
-    
-    curl https://raw.githubusercontent.com/davidkhala/azure-utils/refs/heads/main/cli/entra.sh | bash -s create-service-principal $service_principal | > credential.json
-    export clientsecret=$(jq -r ".password" credential.json)
-    export clientid=$(jq -r ".appId" credential.json)
-    rm credential.json
+export tenantid=$(curl https://raw.githubusercontent.com/davidkhala/azure-utils/refs/heads/main/cli/context.sh | bash -s tenant)
 
-}
-clientid="<CLIENT_ID>"         # Azure Service Principal client ID
-clientsecret="<CLIENT_SECRET>" # Azure Service Principal client secret
-tenantid="<TENANT_ID>"
+credentialFile=".credential.json"
+
+if ! [ -f $credentialFile ]; then
+    # create service principal
+    az ad sp create-for-rbac --name $1 --role Reader --scopes "/subscriptions/$subscription" --query "{appId:appId, password:password}" > $credentialFile
+fi
+export clientid=$(jq -r ".appId" $credentialFile) # Azure Service Principal client ID
+    
+export clientsecret=$(jq -r ".password" $credentialFile) # Azure Service Principal client secret
+
 
 setup() {
     if ! az group exists --resource-group $rg; then
-        az group create --location $region --resource-group $rg
+        az group create --location $purviewlocation --resource-group $rg
     fi
 
-    git clone https://github.com/microsoft/Purview-ADB-Lineage-Solution-Accelerator.git
+    if ! [ -d Purview-ADB-Lineage-Solution-Accelerator ]; then
+        git clone https://github.com/microsoft/Purview-ADB-Lineage-Solution-Accelerator.git
+    fi
+    
 
     # you need to add the service principal to the data curator role in the Purview resource.
 }
