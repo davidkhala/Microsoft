78fe894cec282b5909ff62fff26bff1b1cda6232
David Liu <david-khala@hotmail.com>
2024-10-30 11:40:22 +0800

    Update demo.sh

--- purview/lineage/palsa/demo.sh
+++ purview/lineage/palsa/demo.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 export rg=${rg:-"Purview-ADB-Lineage-Solution-Accelerator"} # resource_group
+if ! az group exists --resource-group $rg; then
+        az group create --location $purviewlocation --resource-group $rg
+fi
 
 export purviewlocation=SoutheastAsia
 export prefix=""
@@ -9,16 +12,4 @@ release=release/2.3
 
 export tenantid=$(curl https://raw.githubusercontent.com/davidkhala/azure-utils/refs/heads/main/cli/context.sh | bash -s tenant)
 
-setup() {
-    if ! az group exists --resource-group $rg; then
-        az group create --location $purviewlocation --resource-group $rg
-    fi
-
-    if ! [ -d Purview-ADB-Lineage-Solution-Accelerator ]; then
-        git clone https://github.com/microsoft/Purview-ADB-Lineage-Solution-Accelerator.git
-    fi
-    
-
-    # you need to add the service principal to the data curator role in the Purview resource.
-}
 $@
