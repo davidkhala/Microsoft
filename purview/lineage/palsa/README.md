27e3541dde0163302e8cb13df889f51ef458cae2
David <david-khala@hotmail.com>
2024-10-31 11:01:11 +0000

    EOD backup

--- purview/lineage/palsa/README.md
+++ purview/lineage/palsa/README.md
@@ -11,9 +11,15 @@ chmod +x openlineage-deployment.sh
 cd -
 ```
 
-Connect with existing
-```
+## Deploy option 2: Connect with existing
+1. Run `./standalone.sh deploy`
+2. Add the service principal `Purview-ADB-Lineage-Solution-Accelerator` to the `Data Curator` role in your Purview resource.
+    - `Data Map` > `Domains` > select the Purview instance > `Role assignments`
+3. Install necessary types into your Purview instance 
+    ```
+    `./standalone.sh config-purview`
+    TODO troubleshoot
+    ```
 
-./standalone.sh
-
-```
+4. TODO 
+https://github.com/microsoft/Purview-ADB-Lineage-Solution-Accelerator/blob/release/2.3/deploy-base.md#download-the-openlineage-spark-agent-and-configure-with-your-azure-databricks-clusters
--- purview/lineage/palsa/standalone.sh
+++ purview/lineage/palsa/standalone.sh
@@ -1,4 +1,5 @@
 set -e
+
 if ! [ -f ./common.sh ]; then
     curl https://raw.githubusercontent.com/davidkhala/Microsoft/refs/heads/main/purview/lineage/palsa/common.sh -O
     source ./common.sh
@@ -7,6 +8,34 @@ else
     source ./common.sh
 fi
 
+purviewName=$(curl https://raw.githubusercontent.com/davidkhala/azure-utils/refs/heads/main/cli/purview.sh | bash -s name)
+
+deploy() {
+    curl https://raw.githubusercontent.com/microsoft/Purview-ADB-Lineage-Solution-Accelerator/refs/heads/release/2.3/deployment/infra/newdeploymenttemp.json -O
+
+    az deployment group create --resource-group $rg --template-file "./newdeploymenttemp.json" --parameters purviewName=$purviewName prefixName= clientid=$clientid clientsecret=$clientsecret resourceTagValues={} --output none
+    # if failure, clean up by `az deployment group delete --name newdeploymenttemp --resource-group $rg`
+    rm newdeploymenttemp.json
+}
+
+# Install necessary types into your Purview instance
+config-purview() {
+    purview_endpoint="https://$purviewName.purview.azure.com"
+
+    tenantid=$(curl https://raw.githubusercontent.com/davidkhala/azure-utils/refs/heads/main/cli/context.sh | bash -s tenant)
+
+    local login_endpoint="https://login.microsoftonline.com/$tenantid/oauth2/token"
+    acc_purview_token=$(curl $login_endpoint --data "resource=https://purview.azure.net&client_id=$clientid&client_secret=$clientsecret&grant_type=client_credentials" -H Metadata:true -s | jq -r '.access_token')
+
+    curl https://raw.githubusercontent.com/microsoft/Purview-ADB-Lineage-Solution-Accelerator/refs/heads/release/2.3/deployment/infra/Custom_Types.json -O
+    
+    curl -s -X POST $purview_endpoint/catalog/api/atlas/v2/types/typedefs -H "Authorization: Bearer $acc_purview_token" -H "Content-Type: application/json" -d @Custom_Types.json
+    # TODO troubleshoot error
+    # {"requestId":"d1fb3a39-9016-44c0-9a7a-8164419d4188","errorCode":"ATLAS-400-00-01A","errorMessage":"invalid parameters: invalid payload, expect schemaAttributes in purview_custom_connector_generic_column should be list of string, but found: data_type"}
+    rm Custom_Types.json
+}
+
+# TODO databricks part
 if ! unzip -v; then
     echo "unzip is required. Please find and install on your OS"
     exit 1
@@ -16,13 +45,5 @@ if ! databricks -v; then
     # install DataBricks CLI
     curl -fsSL https://raw.githubusercontent.com/databricks/setup-cli/main/install.sh | sudo sh
 fi
-
-rg=${rg:-"Purview-ADB-Lineage-Solution-Accelerator"}
-purviewName=$(curl https://raw.githubusercontent.com/davidkhala/azure-utils/refs/heads/main/cli/purview.sh | bash -s name)
-
-# DEBUG
-curl https://raw.githubusercontent.com/microsoft/Purview-ADB-Lineage-Solution-Accelerator/refs/heads/release/2.3/deployment/infra/newdeploymenttemp.json -O
-
-az deployment group create --resource-group $rg --template-file "./newdeploymenttemp.json" --parameters purviewName=$purviewName prefixName= clientid=$clientid clientsecret=$clientsecret resourceTagValues={} --output none
-# if failure, clean up by `az deployment group delete --name newdeploymenttemp --resource-group $rg`
-rm newdeploymenttemp.json
+# TODO databricks part
+$@
