81f92643765e8bf7d218a3e5de67f220cfb4ae4b
David Liu <david-khala@hotmail.com>
2024-10-31 11:54:51 +0800

    Update common.sh

--- purview/lineage/palsa/common.sh
+++ purview/lineage/palsa/common.sh
@@ -1,8 +1,16 @@
 
-rg=${rg:-"Purview-ADB-Lineage-Solution-Accelerator"} # resource_group
-purviewlocation=SoutheastAsia
+export rg=${rg:-"Purview-ADB-Lineage-Solution-Accelerator"} # resource_group
+export purviewlocation=SoutheastAsia
 if ! az group exists --resource-group $rg; then
         az group create --location $purviewlocation --resource-group $rg
 fi
 
+service_principal=${service_principal:-"Purview-ADB-Lineage-Solution-Accelerator"}
+subscription=$(curl https://raw.githubusercontent.com/davidkhala/azure-utils/refs/heads/main/cli/context.sh | bash -s subscription)
+credentialFile=".credential.json"
+
+if ! [ -f $credentialFile ]; then
+    # create service principal
+    az ad sp create-for-rbac --name $service_principal --role Reader --scopes "/subscriptions/$subscription" --query "{appId:appId, password:password}" > $credentialFile
+fi
 
