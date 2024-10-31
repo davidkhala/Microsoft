5550f94cd6dee788c6dd227a9e763732a0194fe7
David Liu <david-khala@hotmail.com>
2024-10-31 11:46:08 +0800

    Update standalone.sh

--- purview/lineage/palsa/standalone.sh
+++ purview/lineage/palsa/standalone.sh
@@ -1,3 +1,3 @@
 rg=${rg:-"Purview-ADB-Lineage-Solution-Accelerator"}
-purviewName=# TODO get
-az deployment group create --resource-group $rg --template-file "./Purview-ADB-Lineage-Solution-Accelerator/deployment/infra/newdeploymenttemp.json" --parameters purviewName=<ExistingPurviewServiceName>
+purviewName=$(curl https://raw.githubusercontent.com/davidkhala/azure-utils/refs/heads/main/cli/purview.sh | bash -s name)
+az deployment group create --resource-group $rg --template-file "./Purview-ADB-Lineage-Solution-Accelerator/deployment/infra/newdeploymenttemp.json" --parameters purviewName=$purviewName
