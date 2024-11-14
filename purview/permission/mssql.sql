2f9a816d5bd43c3411c446968c110de178b4ccb6
David Liu <david-khala@hotmail.com>
2024-11-14 22:47:16 +0800

    Update mssql.sql

--- purview/permission/mssql.sql
+++ purview/permission/mssql.sql
@@ -1,18 +1,17 @@
 -- A good SQL runtime is **Query Editor** in Azure SQL DB db level
 
 -- Create
-
+DECLARE @SAMI VARCHAR(20);
+SET @SAMI = 'admin-david'
 CREATE USER "[SAMI name]" FROM EXTERNAL PROVIDER
-GO
 
 EXEC sp_addrolemember 'db_owner', "[SAMI name]"
-GO
 
 CREATE MASTER KEY
 GO
 
 -- Cleanup  
---  by Transact-SQL syntax
+-- Step 0. in SSMS, go to `Databases` > [database] > `Extended Events` > `Sessions` > stop and delete Purview sessions first
 DECLARE @SAMI VARCHAR(20);
 SET @SAMI = 'admin-david'
 DECLARE @name VARCHAR(91);
@@ -21,7 +20,11 @@ SELECT @name = name
 	FROM sys.database_scoped_credentials 
 	WHERE principal_id = USER_ID(@SAMI);
 
--- TODO syntax error
-DROP DATABASE SCOPED CREDENTIAL @name
--- TODO syntax error
-DROP USER @SAMI
+DECLARE @sql NVARCHAR(MAX);
+
+SET @sql = 'DROP DATABASE SCOPED CREDENTIAL [' + @name + ']';
+EXEC sp_executesql @sql;
+
+SET @sql = 'DROP USER [' + @SAMI + ']';
+EXEC sp_executesql @sql;
+
