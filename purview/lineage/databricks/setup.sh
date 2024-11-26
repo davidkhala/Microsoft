51e2d012eb360cd0b0fdb40cbd19ffa6facf5e71
David Liu <david-khala@hotmail.com>
2024-11-27 00:49:55 +0800

    migrated to databricks-common

--- purview/lineage/databricks/setup.sh
+++
@@ -1,4 +0,0 @@
-# enable system.access table
-METASTORE_ID=$(curl -s https://raw.githubusercontent.com/davidkhala/spark/refs/heads/main/databricks/cli/context.sh | bash -s metastore)
-databricks system-schemas enable $METASTORE_ID access
-
