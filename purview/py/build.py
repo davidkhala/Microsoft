8f4f737aa7c74b61ef22d6c98529cda1c49c5485
David Liu <david-khala@hotmail.com>
2025-02-17 08:16:29 +0800

    backup

---
+++ purview/py/build.py
@@ -0,0 +1,21 @@
+import os.path
+
+from davidkhala.build import Installer
+from davidkhala.syntax.path import dirname
+
+_current = dirname(__file__)
+source = os.path.join(_current, "davidkhala/microsoft/purview/databricks/cli.py")
+i = Installer(os.path.join(_current, 'dist'), source)
+
+
+def build():
+    r = i.build()
+    print(" ".join(r.args)) # raw command
+
+
+def clean():
+    i.clean(True)
+
+
+if __name__ == "__main__":
+    build()
--- purview/py/davidkhala/microsoft/purview/databricks/cli.py
+++ purview/py/davidkhala/microsoft/purview/databricks/cli.py
@@ -5,6 +5,7 @@ from databricks.sdk import WorkspaceClient
 from davidkhala.azure.auth import from_service_principal
 from davidkhala.databricks.workspace import Workspace
 from davidkhala.databricks.workspace.path import SDK
+
 from davidkhala.microsoft.purview.databricks import Databricks
 from davidkhala.microsoft.purview.fabric.powerbi import PowerBI
 from davidkhala.microsoft.purview.lineage import Lineage
@@ -39,41 +40,42 @@ def powerbi_dataset_lineage(credential: TokenCredential, target_dataset: str, st
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Purview tool for Databricks")
+    parser = argparse.ArgumentParser(description="Microsoft Purview tool for Databricks")
 
-    parser.add_argument("--entra.tenant-id", required=True)
-    parser.add_argument("--entra.client-id", required=True)
-    parser.add_argument("--entra.client-secret", required=True)
+    parser.add_argument("--entra.tenant-id", required=True, help="Tenant ID of the Entra Domain")
+    parser.add_argument("--entra.client-id", required=True, help="Application (client) ID of the Service Principal")
+    parser.add_argument("--entra.client-secret", required=True,
+                        help="Value of a Client secret, belonging to the Service Principal")
 
     subparsers = parser.add_subparsers(dest="command", required=True)
 
     # Subparser for the 'rename' action
-    rename_parser = subparsers.add_parser("rename", help="Rename Databricks Notebook asset in Purview")
+    rename_parser = subparsers.add_parser("rename", help="Rename Databricks Notebook asset")
     rename_parser.add_argument("--databricks.host", help="e.g https://adb-2367537008441771.11.azuredatabricks.net")
     rename_parser.add_argument("--databricks.token", help="Databricks token (starting with 'dapi')")
 
     # Subparser for the 'lineage' action
-    lineage_parser = subparsers.add_parser("lineage", help="weave assets in Purview")
-    lineage_parser.add_argument("strategy",choices=['desktop', 'publish'], required=True, help="Publish strategy from Databricks")
+    lineage_parser = subparsers.add_parser("lineage", help="Weave data assets")
+    lineage_parser.add_argument("strategy", choices=['desktop', 'publish'], help="Publish strategy from Databricks")
     lineage_parser.add_argument("--dataset", required=True, help="Targeted PowerBI dataset name")
 
     args = parser.parse_args()
     credential = from_service_principal(
-        getattr(args, 'entra.tenant-id'),
-        getattr(args, 'entra.client-id'),
-        getattr(args, 'entra.client-secret'),
+        getattr(args, 'entra.tenant_id'),
+        getattr(args, 'entra.client_id'),
+        getattr(args, 'entra.client_secret'),
     )
     match args.command:
         case 'rename':
             client = WorkspaceClient(host=getattr(args, 'databricks.host'), token=getattr(args, 'databricks.token'))
+            client.current_user.me() # validate
             rename(Workspace(client), credential)
         case 'lineage':
             strategy = Builder.DatabricksStrategy.Publish
-            match getattr(args, 'strategy'):
-                case 'desktop':
-                    strategy = Builder.DatabricksStrategy.Desktop
+            if getattr(args, 'strategy') == 'desktop':
+                strategy = Builder.DatabricksStrategy.Desktop
 
-            powerbi_dataset_lineage(credential, getattr(args,'dataset'), strategy)
+            powerbi_dataset_lineage(credential, getattr(args, 'dataset'), strategy)
 
 
 if __name__ == "__main__":
